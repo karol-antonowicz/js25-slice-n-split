5e5b70a4bdf0c901a2b6cff9c8ae9662bacc1b90
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-10-05 23:47:42 +0200

    initial commit: added test.js with some simple split n slice functions

---
+++ test.js
@@ -0,0 +1,7 @@
+var str = "hello, world";  //java od 0 sie liczy ;) //
+var str2 = str.slice(2,9);
+console.log(str2);
+
+var tags = "meat, ham, salami, pork, beef, chicken"
+var tagsArray = tags.split(",");
+console.log(tagsArray);
